e1b7094a33b267f198241d58a6693866302132f9
hanif <hnflasting@gmail.com>
2020-07-13 23:49:55 +0700

    menambahkan file destructring object

---
+++ 06_destructing_object.js
@@ -0,0 +1,10 @@
+const profile = {
+	nama: 'Hanif',
+	umur: 19,
+	hobby: [ 'salto', 'lari', 'turu' ]
+};
+
+const { nama, umur, hobby } = profile;
+const [ salto, lari, turu ] = hobby;
+
+console.log(turu);
