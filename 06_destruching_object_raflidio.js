241acf7c6f54bd58c7b17af3a0cb53543a702f6a
rafli-dio <raflidiomuhammadvalent@gmail.com>
2020-07-13 23:49:19 +0700

    menambahkan destruching object

---
+++ 06_destruching_object_raflidio.js
@@ -0,0 +1,9 @@
+const profile = {
+    nama: "raflidio",
+    umur: "16",
+    hobby: ["code", "ngapusi", "turu"],
+};
+
+const { nama, umur} = profile;
+const [code, ngapusi, turu] = profile.hobby;
+console.log(turu);
