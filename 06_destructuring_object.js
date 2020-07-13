a527c601c62f469aff29fe8f366d1ad457c303dc
rygenzx <maulana.zx525@gmail.com>
2020-07-13 23:50:25 +0700

    add file destructuring array js

---
+++ 06_destructuring_object.js
@@ -0,0 +1,10 @@
+const profile = {
+    nama: 'Rigen',
+    umur: 16,
+    hobby: ["tangi", "mangan", "turu"]
+};
+
+const {nama, umur, hobby} = profile;
+const [tangi, mangan, turu] = profile.hobby;
+
+console.log(turu);
