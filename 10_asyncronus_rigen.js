9b4d61b8b2a8c8b363e9318b3fd2a61203496c80
rygenzx <maulana.zx525@gmail.com>
2020-07-14 14:12:47 +0700

    add file asyncronus and syncronus js

---
+++ 10_asyncronus_rigen.js
@@ -0,0 +1,9 @@
+//  syncronus
+console.log("satu");
+console.log("dua");
+console.log("tiga");
+
+//  Asyncronus
+setTimeout(() => console.log("satu"), 3000);
+setTimeout(() => console.log("dua"), 1000);
+setTimeout(() => console.log("tiga"), 2000);
