653935ce3756e407765e8c0aca56b7be2888c364
rafli-dio <raflidiomuhammadvalent@gmail.com>
2020-07-14 14:13:15 +0700

    asyn

---
+++ 10_asyncronus_dio.js
@@ -0,0 +1,9 @@
+console.log("satu");
+console.log("dua")
+console.log("tiga")
+
+// asyncronus
+
+setTimeout({} => console.log("satu"), 3000)
+setTimeout({} => console.log("dua"), 1000)
+setTimeout({} => console.log("tiha"), 2000)
