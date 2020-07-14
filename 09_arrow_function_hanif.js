db7f79494446f8ec19ad256d7c816735f9be380e
hanif <hnflasting@gmail.com>
2020-07-14 14:01:08 +0700

    menambahkan file arrow function

---
+++ 09_arrow_function_hanif.js
@@ -0,0 +1,11 @@
+//Arrow function
+const coba = (name, umur) => {
+	return `Nama saya ${name}, Umur saya ${umur}`;
+};
+
+console.log(coba('Hanif', '19'));
+
+//Arrow function expresion
+console.log(() => {
+	return 'hanif';
+});
