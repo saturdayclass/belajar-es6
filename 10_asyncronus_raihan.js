ad3f8ba801d81614aafa4041e9ff291a817ad40a
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-07-14 14:12:10 +0700

    menambahkan file asyncronus

---
+++ 10_asyncronus_raihan.js
@@ -0,0 +1,9 @@
+// Syncronus
+// console.log("Satu");
+// console.log("Dua");
+// console.log("Tiga");
+
+// Asyncronus
+setTimeout(() => console.log("satu"), 3000);
+setTimeout(() => console.log("dua"), 1000);
+setTimeout(() => console.log("tiga"), 2000);
