2d49a1ac7673f249abe950b8808d49188ce50dc8
rygenzx <maulana.zx525@gmail.com>
2020-07-14 14:02:43 +0700

    add file arrow function html

---
+++ 09_arrow_function.js
@@ -0,0 +1,12 @@
+//  arrow function declaration
+const profile = (name) => {
+    return `jenengku ${name}`
+}
+
+console.log(profile("Rigen"))
+
+//  arrow function expression
+
+console.log(() => {
+    console.log("Maulana");
+});
