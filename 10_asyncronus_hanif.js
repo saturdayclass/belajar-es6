d9fc56516043659a588617359736895a8795b551
hanif <hnflasting@gmail.com>
2020-07-14 14:12:22 +0700

    menambahkan file asyncronus file

---
+++ 10_asyncronus_hanif.js
@@ -0,0 +1,9 @@
+//syncronus
+// console.log('Satu');
+// console.log('Dua');
+// console.log('Tiga');
+
+//asyncronus
+setTimeout(() => console.log('satu'), 3000);
+setTimeout(() => console.log('dua'), 1000);
+setTimeout(() => console.log('tiga'), 2000);
