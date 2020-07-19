0bef7767f623b2e4a3c213a947db011672d61b7a
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-07-19 14:40:44 +0700

    menambahkan file fetch async await

---
+++ 12_fetch_raihan.js
@@ -0,0 +1,20 @@
+const element = document.getElementById("hasil");
+
+const requestApi = async () => {
+  try {
+    // request ke api dengan method GET
+    const req = await fetch(
+      "https://natours-raihan.herokuapp.com/api/v1/tours"
+    );
+
+    // convert hasil request ke API ke json
+    const res = await req.json();
+
+    // memasukan hasil convert json ke dalam element html
+      element.innerHTML = `<h1>${res.data.data[0].startLocation.address}</h1>`;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+requestApi();
